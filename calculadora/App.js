a9d40ca6a47730f36180b41afff331c44bc57693
LucasSpricigo <lucasspricigo21@gmail.com>
2026-03-13 11:39:34 -0300

    finalização do projeto da calculadora inserindo os calculos

--- calculadora/App.js
+++ calculadora/App.js
@@ -49,6 +49,35 @@ export default function App() {
     }
   }
 
+  const handleEquals = () => {
+    const inputValue = parseFloat(display);
+
+    if(previousValue != null && operator) {
+      const result = calculate(previousValue, inputValue, operator);
+      setDisplay(String(result));
+      setPreviousValue(null);
+      setOperator(null);
+      setWaitingForNewValue(true);
+    }
+  }
+
+  const handlePercentage = () => {
+    setDisplay(String(parseFloat(display) / 100));
+  }
+
+  const handleDecimal = () => {
+    if(waitingForNewValue) {
+      setDisplay('0.');
+      setWaitingForNewValue(false);
+    } else if (display.indexOf('.') === -1) {
+      setDisplay(display + '.');
+    }
+  }
+
+  const handleToggleSign = () => {
+    setDisplay(String(parseFloat(display) * -1));
+  }
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
@@ -64,13 +93,13 @@ export default function App() {
         <TouchableOpacity style={styles.functionButton} onPress={handleClear}>
           <Text style={styles.functionText}>C</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.functionButton}>
+        <TouchableOpacity style={styles.functionButton} onPress={handleToggleSign}>
           <Text style={styles.functionText}>+/-</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.functionButton}>
+        <TouchableOpacity style={styles.functionButton} onPress={handlePercentage}>
           <Text style={styles.functionText}>%</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.operatorButton}>
+        <TouchableOpacity style={styles.operatorButton} onPress={() => handleOperatorPress('÷')}>
           <Text style={styles.operatorText}>÷</Text>
         </TouchableOpacity>
       </View>
@@ -85,7 +114,7 @@ export default function App() {
         <TouchableOpacity style={styles.buttonNumber} onPress={() => handleNumberPress(9)}>
           <Text style={styles.buttonText}>9</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.operatorButton}>
+        <TouchableOpacity style={styles.operatorButton} onPress={() => handleOperatorPress('*')}>
           <Text style={styles.operatorText}>x</Text>
         </TouchableOpacity>
       </View>
@@ -100,7 +129,7 @@ export default function App() {
         <TouchableOpacity style={styles.buttonNumber} onPress={() => handleNumberPress(6)}>
           <Text style={styles.buttonText}>6</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.operatorButton}>
+        <TouchableOpacity style={styles.operatorButton} onPress={() => handleOperatorPress('-')}>
           <Text style={styles.operatorText}>-</Text>
         </TouchableOpacity>
       </View>
@@ -115,7 +144,7 @@ export default function App() {
         <TouchableOpacity style={styles.buttonNumber} onPress={() => handleNumberPress(3)}>
           <Text style={styles.buttonText}>3</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.operatorButton}>
+        <TouchableOpacity style={styles.operatorButton} onPress={() => handleOperatorPress('+')}>
           <Text style={styles.operatorText}>+</Text>
         </TouchableOpacity>
       </View>
@@ -124,10 +153,10 @@ export default function App() {
         <TouchableOpacity style={[styles.buttonNumber, styles.doubleWidthButton]} onPress={() => handleNumberPress(0)}>
           <Text style={styles.buttonText}>0</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.buttonNumber}>
+        <TouchableOpacity style={styles.buttonNumber} onPress={handleDecimal}>
           <Text style={styles.buttonText}>.</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.operatorButton}>
+        <TouchableOpacity style={styles.operatorButton} onPress={handleEquals}>
           <Text style={styles.operatorText}>=</Text>
         </TouchableOpacity>
       </View>
